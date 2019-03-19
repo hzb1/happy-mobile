e361f13e958332b290782b4b6a9c9ea7fcfc3123
huzhibin <2285901508@qq.com>
2019-03-19 21:04:54 +0800

    修改Toast演示代码3

--- example/js/dist/bundle.js
+++ example/js/dist/bundle.js
@@ -92,7 +92,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "663104d2d28fc7c800f2";
+/******/ 	var hotCurrentHash = "9c193f0a1e22485abc6e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
