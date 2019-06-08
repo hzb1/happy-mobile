c2d97778834fcbd433764a4daee99ec04a53d89e
huzhibin <2285901508@qq.com>
2019-06-08 19:57:28 +0800

    提交

--- docs/config.js
+++ docs/config.js
@@ -5,7 +5,7 @@ export const navConfig = cList
 
 console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 console.log('navConfig', navConfig)
-export const iframeBaseUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:7777' : '' // production development
+export const iframeBaseUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:7777' : 'https://hzb1.github.io/happy-mobile/example/js/dist' // production development
 
 // export default {
 //   navConfig,
